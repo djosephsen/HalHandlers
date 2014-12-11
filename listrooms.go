2b0de8e5e3385efdc050360e7174f5b7da53e363
Dave Josephsen <dave@skeptech.org>
2014-12-11 10:46:02 -0600

    better pattern

--- listrooms.go
+++ listrooms.go
@@ -7,7 +7,7 @@ import (
 
 var Listrooms = &hal.Handler{
 	Method:  hal.RESPOND,
-	Pattern: `what room is this`,
+	Pattern: `(what room)|(list *room)|(room *list)`,
 	Run: func(res *hal.Response) error {
 		room := res.Message.Room
 		reply := fmt.Sprintf("Current room is: %s",room)
