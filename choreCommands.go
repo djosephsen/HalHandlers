b12fb86b45e1996fa3cd08201f3323b2b364cc19
Dave Josephsen <dave@skeptech.org>
2014-12-11 12:02:42 -0600

    builds

--- choreCommands.go
+++ choreCommands.go
@@ -3,9 +3,6 @@ package bothandlers
 import (
 	"fmt"
 	"github.com/djosephsen/hal"
-	"math/rand"
-	"time"
-	"strings"
 )
 
 var ListChores = &hal.Handler{
@@ -13,7 +10,7 @@ var ListChores = &hal.Handler{
 	Pattern: `(list chores)|(chore list)`,
 	Run: func(res *hal.Response) error {
 		var reply string
-		for _,c := range res.Robot.chores{
+		for _,c := range res.Robot.Chores{
 			reply = fmt.Sprintf("%s\n%s:%s:%s:%s",reply,c.Name, c.Schedule, c.Next, c.State)
 		}
 		return res.Reply(reply)
