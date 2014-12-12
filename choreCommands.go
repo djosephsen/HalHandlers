4a84a131df808b0cc541e0b4fee1402420352b43
Dave Josephsen <dave@skeptech.org>
2014-12-11 21:40:57 -0600

    renamed schedule

--- choreCommands.go
+++ choreCommands.go
@@ -15,7 +15,7 @@ var ListChores = &hal.Handler{
 		}else{
 			reply=`Name  :small_blue_diamond:  Schedule  :small_blue_diamond:  Next Run  :small_blue_diamond: Current State`
 			for _,c := range res.Robot.Chores{
-				reply = fmt.Sprintf("%s\n%s:small_blue_diamond:%s:small_blue_diamond:%s:small_blue_diamond:%s",reply,c.Name, c.Schedule, c.Next, c.State)
+				reply = fmt.Sprintf("%s\n%s:small_blue_diamond:%s:small_blue_diamond:%s:small_blue_diamond:%s",reply,c.Name, c.Sched, c.Next, c.State)
 			}
 		}
 		return res.Reply(reply)
