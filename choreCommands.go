9f92a3015d5c305004db771638c01c8eb0f2292b
Dave Josephsen <dave@skeptech.org>
2014-12-11 21:43:42 -0600

    merging listrooms into choreCommands

--- choreCommands.go
+++ choreCommands.go
@@ -21,3 +21,13 @@ var ListChores = &hal.Handler{
 		return res.Reply(reply)
 	},
 }
+
+var ListRooms = &hal.Handler{
+	Method:  hal.RESPOND,
+	Pattern: `(what room)|(list *room)|(room *list)`,
+	Run: func(res *hal.Response) error {
+		room := res.Message.Room
+		reply := fmt.Sprintf("Current room is: %s",room)
+		return res.Send(reply)
+	},
+}
