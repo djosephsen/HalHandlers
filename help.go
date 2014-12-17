22f833cc3678407891a8a670feb25b2a84bebbc4
Dave Josephsen <dave@skeptech.org>
2014-12-17 15:45:31 -0600

    working but not for fullhandlers

--- help.go
+++ help.go
@@ -8,16 +8,23 @@ import (
 
 var Help = &hal.Handler{
 	Method:  hal.HEAR,
-	Pattern: `help (.*)`,
+	Pattern: `help`,
 	Usage: `help: prints this message when you type "botname help"`,
 	Run: func(res *hal.Response) error {
 		var reply string
-		handlers:=hal.Handlers
+		handlers:=res.Robot.Handlers()
+		HandlerType:=reflect.ValueOf(new(hal.Handler)).Elem()
+		//I can't reflect out the fullHandler type becaues hal doesn't export it
+		//so this plugin can only print the usage of hal.Handler's
+		//FullHandlerType:=reflect.ValueOf(new(hal.FullHandler)).Elem()
 		for _,h:=range handlers{
-			hval:=reflect.ValueOf(h)
-			usage:=hval.FieldByName(`usage`)
-			reply=fmt.Sprintf("%s\n%s",reply,usage)
+			hval:=reflect.ValueOf(h).Elem()
+			hal.Logger.Debug("this hval is a ",hval.Type())
+			if hval.Type() == HandlerType.Type(){
+					usage := hval.FieldByName(`Usage`)
+					reply=fmt.Sprintf("%s\n%s",reply,usage)
 			}
+		}
 		return res.Send(reply)
 	},
 }
